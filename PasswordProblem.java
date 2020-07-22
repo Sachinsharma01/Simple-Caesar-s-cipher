7b33f305efa1f35766773be9f37859a91b7bcda7
Sachin Sharma <67816867+Sachinsharma01@users.noreply.github.com>
2020-07-23 02:49:57 +0530

    Remove StringBuilder Class
    I just remove the StringBuilder class and add a String for the ciphered password which makes the program pretty basic and understandable.

--- PasswordProblem.java
+++ PasswordProblem.java
@@ -5,31 +5,21 @@
  *   File: PasswordProblem.java
  */
 // que-> create Caesar Cipher Password as
-        // 1) change all uppercase to lowercase
-        // 2) char = char + 2
+// 1) change all uppercase to lowercase
+// 2) char = char + 2
 package myPractice;
 import java.util.Scanner;
-public class PasswordProblem {
+class PasswordProblem {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.println("Please Enter Your Password");
-        String inputPassword = scn.nextLine();
-        StringBuilder pass = new StringBuilder("");
-        String lowerCase = inputPassword.toLowerCase();
-        for (int i = 0; i < inputPassword.length(); i++) {
-            int p = (int) (lowerCase.charAt(i));
-            int pa = p + 2;
-            char pas = (char) pa;
-            if (p == 122) {
-                int z = 122 - 26;
-                int s = z + 2;
-                char t = (char)s;
-                pass.append(t);
-            } else {
-                pass.append(pas);
-            }
+        System.out.print("Please Enter Your Password : ");
+        String inputPassword = scn.nextLine().toLowerCase();
+        char []inputPasswordArray = inputPassword.toCharArray();
+        String pass = "";
+        for (char i : inputPasswordArray){
+            int value = (int) (i) + 2;
+            pass += (char) (value);
         }
-        System.out.println("Its Caesar Cipher Code is \n"+pass);
+        System.out.print("It's Caesar Cipher Code is : "+pass);
     }
 }
-
